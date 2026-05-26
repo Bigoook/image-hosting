d25d4b23c9d9a933855a079dc8132b3063d48319
Vitaliy <=>
2026-05-26 22:05:33 +0300

    - добавлено config, ImageRepository, logger - Змінено frontend names (per_page -> limit), urls (location.origin -> api url) - 8080 -> 80 port

--- backend/app.py
+++ backend/app.py
@@ -1,45 +1,103 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
 import json
 
+from utils.encoders import AppJSONEncoder
+from logger import logger
+from database import ImageRepository
+
+
 
 class ImageAPIServer(BaseHTTPRequestHandler):
-
-
-    def handle_images(self):
-        self.send_response(200)
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
         
+        self.repo = ImageRepository()
+        self.add_images()
+        
+    def add_images(self):
+        if self.repo.list():
+            logger.info("Images already exist in the database, skipping seeding")
+            return  
+        
+        images = [
+            {
+                "filename": "image1.jpg",
+                "original_name": "photo1.jpg",
+                "size": 1024,
+                "file_type": "image/jpeg"
+            },
+            {
+                "filename": "image2.png",
+                "original_name": "photo2.png",
+                "size": 2048,
+                "file_type": "image/png"
+            },
+            {
+                "filename": "image3.png",
+                "original_name": "photo3.png",
+                "size": 150,
+                "file_type": "image/png"
+            },
+            {
+                "filename": "image3.png",
+                "original_name": "photo3.png",
+                "size": 200,
+                "file_type": "image/jpeg"
+            },
+        ]
+        
+        for image in images:
+            image_id = self.repo.create(
+                filename=image["filename"],
+                original_name=image["original_name"],
+                size=image["size"],
+                file_type=image["file_type"]
+            )
+            logger.info(f"Added image with ID {image_id} to the database")
+
+
+    def handle_images(self): # /api/images
+        self.send_response(200)
         self.send_header("Content-type", "application/json")
         self.end_headers()
 
-        response_data = {"status": "success", "message": "Hello from Python!"}
-        json_string = json.dumps(response_data)
+        images = self.repo.list()
+        
+        self.wfile.write(json.dumps(images, cls=AppJSONEncoder).encode('utf-8'))
 
-        self.wfile.write(json_string.encode('utf-8'))
-    
-    def handle_image(self):
-        ...
-
+        self.rfile.read()
 
     def handle_upload(self):
+        ...
+        
+        
+    def delete_image(self):
         ...
 
 
     def do_GET(self):
+        logger.info(f"Received GET request for {self.path}")
         self.path = self.path.rstrip("/")
 
         if self.path == "/images":
             self.handle_images()
-        elif "/images/" in self.path: # FIXME
-            self.handle_image()
         
 
     def do_POST(self):
+        logger.info(f"Received POST request for {self.path}")
+        
         if self.path == "/upload":
             self.handle_upload()
             
     
+    def do_DELETE(self):
+        logger.info(f"Received DELETE request for {self.path}")
+        if self.path.startswith("/images/"):
+            self.delete_image()
+            
+    
 if __name__ == "__main__":
-    server = HTTPServer(("localhost", 8000), ImageAPIServer)
+    server = HTTPServer(("0.0.0.0", 8000), ImageAPIServer)
     
     try:
         print("Сервер запущено...")
